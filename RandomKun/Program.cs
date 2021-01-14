a8350e82005c5643b48e1972bb91ec7dd72b353d
AntiqueR <antique.pendulum@gmail.com>
2021-01-14 13:40:04 +0900

    Initial commit

---
+++ RandomKun/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace RandomKun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = new byte[sizeof(UInt16)];
+            using var rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(data);
+            var n = BitConverter.ToUInt16(data, 0);
+
+            Console.WriteLine(n);
+            Console.ReadKey();
+        }
+    }
+}
